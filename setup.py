bafcdf8098a53f18bff35f19a76973dc16bc662d
Bruno Morais <brunosmmm@gmail.com>
2017-09-22 15:59:35 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+"""Setup."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="hbussd",
+    version="0.1",
+    packages=find_packages(),
+
+    install_requires=['Twisted>=17.5.0',
+                      'bottle>=0.12.13',
+                      'pyserial>=3.4',
+                      'simplejson>=3.11.1',
+                      'txJSON-RPC>=0.5'],
+
+    author="Bruno Morais",
+    author_email="brunosmmm@gmail.com",
+    description="HBUS services daemon",
+
+    scripts=['hbussd.py'],
+)
