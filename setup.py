ffe4e939639410b619a27efc2b98bd92ee787211
Bruno Morais <brunosmmm@gmail.com>
2017-09-24 09:28:27 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ from setuptools import setup, find_packages
 setup(
     name="hbussd",
     version="0.1",
-    packages=find_packages(),
-    include_package_data=True,
+    packages=find_packages('hbussd'),
+    package_dir={'': 'hbussd'},
+    package_data={'': ['data/*']},
 
     install_requires=['Twisted>=17.5.0',
                       'bottle>=0.12.13',
